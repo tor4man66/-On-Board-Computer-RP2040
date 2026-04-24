31f0d784366194737fedbf3ecb3d8e6f042c7a4f
tor4man <aleksandrchmirkov@gmail.com>
2026-04-24 19:52:42 +0300

    Add files via upload

---
+++ Adafruit_GFX_Library/Fonts/all_fonts.h
@@ -0,0 +1,11 @@
+#ifndef ALL_FONTS_H
+#define ALL_FONTS_H
+
+#include <Arduino.h>
+#include <Adafruit_GFX.h>
+
+#include "FreeSans10.h"
+#include "verdanabUkr6.h"
+#include "FreeSans9pt7b.h"
+
+#endif
